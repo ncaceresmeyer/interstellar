fea1c9e5806cd2776f06ef059febfbe9e79b3edc
natacha caceres meyer <natachacaceresmeyer@natachas-MacBook-Pro.local>
2019-04-23 09:37:22 -0300

    uploading gallery with lazy loading

--- iss-app/src/components/placesGallery.js
+++ iss-app/src/components/placesGallery.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import LazyLoad from 'react-lazyload';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 class PlacesGallery extends Component {
 	state = {
-		mediaPlaces: []
+		mediaPlaces: [],
 	};
 
 	async componentDidMount() {
-	    const { data: mediaPlaces } = await axios.get("https://pixabay.com/api/", {params: {key: process.env.REACT_APP_PIXKEY, q:"cities", image_type: "photo", category: "places", per_page: "15", order: "latest"}})
+	    const { data: mediaPlaces } = await axios.get('https://pixabay.com/api/', 
+	    	{ params: {
+	    		key: process.env.REACT_APP_PIXKEY, 
+	    		q: 'Cities', 
+	    		image_type: 'photo', 
+	    		category: 'places',
+	    		per_page: '15', 
+	    		order: 'latest' }
+	    })
 	    this.setState({ 
           mediaPlaces: mediaPlaces.hits
         });
+
 	}
 
 	render() {
 		return (
 			<div className="iss-container iss-galleryPlaces">
 				<div className="iss-wrapper">
+
 					<h3>Latest Cities Gallery</h3>
 
 					<ul>{this.state.mediaPlaces.map(place => (
 						<li key={ place.id }>
-							<img src={ place.webformatURL } alt={ place.pageURL } />
+							<LazyLoad height={ '100%' }>
+								<TransitionGroup component={ null } appear={ true } enter={ true }>
+									<CSSTransition 
+										key={ place.id }
+						            	timeout={ 200 }
+						            	classNames="fade"
+						            	enter={ true }
+						            	appear={ true }
+						            	in> 
+										<img src={ place.webformatURL } alt={ place.pageURL } />
+									</CSSTransition>
+								</TransitionGroup>
+							</LazyLoad>
 						</li>
 					))}</ul>
 					
@@ -30,5 +54,7 @@ class PlacesGallery extends Component {
 		);
 	}
 }
+
+// in was = this.props.mediaPlaces 
 
 export default PlacesGallery;
