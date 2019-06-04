74542d5fb7a45e35af9ab2b32206cdeb7a87a535
natacha caceres meyer <natachacaceresmeyer@natachas-mbp.agfamonotype.org>
2019-06-04 09:18:00 -0300

    getting user info from image api

--- config/config.js
+++ config/config.js
@@ -5,12 +5,14 @@ module.exports = {
     APP_APIURL: 'http://localhost:3001',
     API_ISSURL:'http://api.open-notify.org/iss-now',
     API_CITIESURL:'http://getnearbycities.geobytes.com/GetNearbyCities?',
-    API_PIXURL:'https://pixabay.com/api/?'
+    API_PIXURL:'https://pixabay.com/api/?',
+    USR_PIXURL: 'https://pixabay.com/users/'
   },
   production: {
     APP_APIURL: 'https://interstellar-app.herokuapp.com/',
     API_ISSURL:'http://api.open-notify.org/iss-now',
     API_CITIESURL:'http://getnearbycities.geobytes.com/GetNearbyCities?',
-    API_PIXURL:'https://pixabay.com/api/?'
+    API_PIXURL:'https://pixabay.com/api/?',
+    USR_PIXURL: 'https://pixabay.com/users/'
   },
 }[confEnv];
