9c383ab4cc2468561bf58bea9bf6efad0213c7df
natacha caceres meyer <natachacaceresmeyer@natachas-mbp.agfamonotype.org>
2019-06-04 09:18:58 -0300

    adding user info to image and css changes

--- iss-app/src/components/placesGallery.js
+++ iss-app/src/components/placesGallery.js
@@ -9,7 +9,7 @@ export default class PlacesGallery extends Component {
 	    this.state = {
 	    	mediaPlaces: [],
 	    	nearbyCities: []
-		}
+		};
     };
 
 	dataPlacesGallery() {
@@ -34,10 +34,12 @@ export default class PlacesGallery extends Component {
 		const { mediaPlaces, mediaSearchTerm, error, nearbyCities } = this.state;
 
 		return (
-			<div className="iss-container iss-galleryPlaces">
+
+			<div className="iss-container iss-galleryPlaces"> 
+
 				<div className="iss-wrapper">
 
-					<h2>Latest Cities Gallery</h2>
+					<h3>Latest Cities Gallery</h3>
 
 					<ul>
 						{nearbyCities.length > 1 ? nearbyCities.map((place, i) =>
@@ -49,9 +51,9 @@ export default class PlacesGallery extends Component {
 
 					{ error && <p className="iss-alertMsg">{ error }</p> }
 
-					<ul className="iss-grid" aria-label="Image Gallery">{ mediaSearchTerm !== undefined
+					<ul className="iss-grid">{ mediaSearchTerm !== undefined
 						? mediaPlaces.map(media => 
-							<li key={ media.id }>
+							<li key={ media.id } tabIndex="0">
 								<LazyLoad height={ '100%' }>
 									<TransitionGroup component={ null } appear={ true } enter={ true }>
 										<CSSTransition 
@@ -61,9 +63,12 @@ export default class PlacesGallery extends Component {
 							            	enter={ true }
 							            	appear={ true }
 							            	in> 
-											<img src={ media.webformatURL } alt={ media.tags } aria-label={ media.tags } />
+											<React.Fragment>
+												<img src={ media.webformatURL } alt={ media.tags } aria-label={ 'image tagged with ' + media.tags } />
+												<span className="iss-grid-info"><p>{ mediaSearchTerm } <a href={ media.url } aria-label={ 'uploaded by user' +  media.user } target="_blank"  rel="noopener noreferrer">{ '@' + media.user }</a></p></span>
+											</React.Fragment>
 										</CSSTransition>
-									</TransitionGroup>
+									</TransitionGroup>	
 								</LazyLoad>
 							</li>)
 						: ( <li className="iss-alertMsg">Duh, we can't find photos for this place...</li> )
